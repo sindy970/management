0d3b68d28e905944f39c200f9e8e99442a68efd5
seunghee.han <seunghee.han@emotion.co.kr>
2025-02-14 20:07:11 +0900

    id 공백인 경우 처리

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -5,38 +5,49 @@ import { useRouter } from "next/navigation";
 
 export default function SignUpPage() {
     const [loading, setLoading] = useState(false);
-    const [idLoading, setIdLoading] = useState(false);
+    const [idLoading, setIdLoading] = useState(null);
     const router = useRouter();
 
     const idChackSubmit = async (event: FocusEvent<HTMLElement>) => {
-
         const idInput = event.target as HTMLInputElement;
-console.log(idInput.value)
-            const response = await fetch(`/api/auth?id=${idInput.value}`, {
-                method: "GET",
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Cache-Control': 'no-cache, no-store, must-revalidate',
-                    Pragma: 'no-cache',
-                    Expires: '0'
-                },
-            });
 
-            if (response.status === 200) {
-                setIdLoading(true);
-            }
+        if(idInput.value.trim().length === 0){
+            setIdLoading(null);
+            return ;
+        }
+
+        const response = await fetch(`/api/auth?id=${idInput.value}`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'Cache-Control': 'no-cache, no-store, must-revalidate',
+                Pragma: 'no-cache',
+                Expires: '0'
+            },
+        });
+
+        if (response.status === 200) {
+            setIdLoading(true);
+        } else {
+            setIdLoading(false);
+        }
     };
 
     const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         setLoading(true);
 
+        const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
         const formData = new FormData(event.currentTarget);
         const data = Object.fromEntries(formData);
 
         if(!idLoading){
             alert("사용 불가한 아이디 입니다.");
             setLoading(false);
+            return event.target.querySelector("#id").focus();
+        }
+        if(!passwordRegex.test(data.id)){
+            alert("영문, 숫자 8자 이상 입력해주세요");
             return event.target.querySelector("#id").focus();
         }
 
@@ -62,35 +73,37 @@ console.log(idInput.value)
 
     return (
         <>
-                <div>
-                    <h1>회원 가입</h1>
-                    <form onSubmit={handleSubmit}>
-                        <div className="flex flex-col gap-2 mt-8">
-                            <label htmlFor="id">ID</label>
-                            <input id="id" name="id" onBlur={idChackSubmit} placeholder="영문, 숫자 8자 이상 입력해주세요" required />
-                            {idLoading ?
-                                <label>사용 가능한 아이디 입니다.</label> :
-                                <label>사용 불가한 아이디 입니다.</label>
-                            }
+            <div>
+                <h1>회원 가입</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="flex flex-col gap-2 mt-8">
+                        <label htmlFor="id">ID</label>
+                        <input id="id" name="id" onBlur={idChackSubmit} placeholder="영문, 숫자 8자 이상 입력해주세요" required />
+                        {idLoading ?
+                            <label>사용 가능한 아이디 입니다.</label> :
+                            idLoading === null ?
+                                null   :
+                                <label style={{color : "red"}}>사용 불가한 아이디 입니다.</label>
+                        }
 
-                            <label htmlFor="password">Password</label>
-                            <input
-                                type="password"
-                                name="password"
-                                placeholder="Your password"
-                                minLength={6}
-                                required
-                            />
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Your password"
+                            minLength={6}
+                            required
+                        />
 
-                            <label htmlFor="name">이름</label>
-                            <input name="name" placeholder="이름" required />
+                        <label htmlFor="name">이름</label>
+                        <input name="name" placeholder="이름" required />
 
-                            <button type="submit" disabled={loading}>
-                                {loading ? "Signing up..." : "Sign up"}
-                            </button>
-                        </div>
-                    </form>
-                </div>
+                        <button type="submit" disabled={loading}>
+                            {loading ? "Signing up..." : "Sign up"}
+                        </button>
+                    </div>
+                </form>
+            </div>
         </>
     );
 }
